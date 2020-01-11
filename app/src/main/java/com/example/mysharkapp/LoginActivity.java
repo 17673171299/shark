c8f9fb96cb28fc8fcf1312b92d57d12f0a3ecff0
yanjialong <1543853606@qq.com>
2020-01-11 17:28:40 +0800

    two

--- app/src/main/java/com/example/mysharkapp/LoginActivity.java
+++ app/src/main/java/com/example/mysharkapp/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.mysharkapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,7 +15,7 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private PopupWindow popupWindow;
 
@@ -31,40 +33,85 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showPopupWindow();
+                lightoff();
             }
         });
     }
 
+
     private void showPopupWindow() {
         //设置ContentView
         View contentView = LayoutInflater.from(this).inflate(R.layout.layout_popupwindow,null);
-        popupWindow = new PopupWindow(contentView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        popupWindow = new PopupWindow(contentView, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        //设置背景图片，必须设置，不然动画没作用
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setFocusable(true);
+
+        //点击屏幕其他地方弹出框消失
+        popupWindow.setOutsideTouchable(true);
+
         popupWindow.setContentView(contentView);
         //设置各个控件的点击响应事件
         TextView zhuc = (TextView) contentView.findViewById(R.id.tv_zhuc);
         TextView sjdenglu = (TextView)contentView.findViewById(R.id.tv_shuojidenglu);
         TextView quxiao = (TextView)contentView.findViewById(R.id.tv_quxiao);
+        zhuc.setOnClickListener(this);
+        sjdenglu.setOnClickListener(this);
+        quxiao.setOnClickListener(this);
+        popupWindow.setOnDismissListener(new poponDismissListener());
         //显示PopupWindow
         View rootview = LayoutInflater.from(LoginActivity.this).inflate(R.layout.activity_login,null);
-        popupWindow.showAtLocation(rootview, Gravity.BOTTOM,0,0);
+        popupWindow.showAtLocation(rootview, Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL,0,0);
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        int id = v.getId();
-//        switch (id) {
-//            case R.id.tv_zhuc :{
-//                Toast.makeText(this, "你点击了注册", Toast.LENGTH_SHORT).show();
-//                popupWindow.dismiss();
-//            }
-//            case R.id.tv_shuojidenglu:{
-//                Toast.makeText(this, "你点击了手机登录", Toast.LENGTH_SHORT).show();
-//                popupWindow.dismiss();
-//            }
-//            case R.id.tv_quxiao:{
-//                Toast.makeText(this, "你点击了取消", Toast.LENGTH_SHORT).show();
-//                popupWindow.dismiss();
-//            }
-//        }
-//    }
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+        switch (id) {
+            case R.id.tv_zhuc :{
+                Toast.makeText(this, "你点击了注册", Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+                lighton();
+                break;
+            }
+            case R.id.tv_shuojidenglu:{
+                Intent register = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(register);
+                popupWindow.dismiss();
+                lighton();
+                break;
+            }
+            case R.id.tv_quxiao:{
+                popupWindow.dismiss();
+                lighton();
+                break;
+            }
+        }
+
+
+    }
+
+    //设置手机屏幕正常
+    private void lighton() {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = 1f;
+        getWindow().setAttributes(lp);
+    }
+
+    //设置手机屏幕变暗
+    private void lightoff() {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = 0.3f;
+        getWindow().setAttributes(lp);
+    }
+
+    private class poponDismissListener implements PopupWindow.OnDismissListener {
+        @Override
+        public void onDismiss() {
+            lighton();
+        }
+    }
 }
